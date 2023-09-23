a9bb3efb47c87ecbb8bb93a5f8bc981228be24c2
Ankit Srivastava <ankitsrivastava0103@gmail.com>
2023-09-23 12:56:26 +0530

    PropTypes Validation & Tailwind CSS Setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import Button from "./Button";
+
 const App = () => {
-    return (
-        <div>App</div>
-    )
-}
+  return (
+    <div>
+      <div>
+        <Button>Click Me!!</Button>
+      </div>
+      <div>
+        <Button>Buy Now!</Button>
+      </div>
+      <div>
+        <Button>See Deal!</Button>
+      </div>
+      <div>
+        <Button>Hid Adds!</Button>
+      </div>
+      <div>
+        <Button>Something</Button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
