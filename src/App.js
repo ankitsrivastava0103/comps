f83992e6edd61027acf715b6c53eb86d93eee8e4
Ankit Srivastava <ankitsrivastava0103@gmail.com>
2023-10-01 12:14:56 +0530

    adding resuable navigation component with context system

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import { useState } from "react";
-import Dropdown from "./components/Dropdown";
+import Route from "./components/Route";
+import AccordionPage from "./pages/AccordionPage";
+import DropdownPage from "./pages/DropdownPage";
+import ButtonPage from "./pages/ButtonPage";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
-  const [selection, setSelection] = useState(null);
-
-  const handleSelectClick = (option) => {
-    setSelection(option);
-  };
-
-  const options = [
-    { label: "Red", value: "red" },
-    { label: "Green", value: "green" },
-    { label: "Blue", value: "blue" },
-  ];
-
   return (
-    <div>
-      <Dropdown
-        options={options}
-        value={selection}
-        onChange={handleSelectClick}
-      />
+    <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+      <Sidebar />
+      <div className="col-span-5">
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route path="/">
+          <DropdownPage />
+        </Route>
+        <Route path="/buttons">
+          <ButtonPage />
+        </Route>
+      </div>
     </div>
   );
 };
